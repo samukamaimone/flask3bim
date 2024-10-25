f189358b1c60b624f2291c99c02f81aeb62f118c
samukamaimone <samuelmaimone55@gmail.com>
2024-10-25 08:36:46 -0300

    aula de 25/10

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, request, flash, redirect, Blueprint
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'StRiNgQNgMSaBe'
+conexao = "mysql+pymysql://alunos:cefetmg@127.0.0.1/flask3bim"
+app.config['SQLALCHEMY_DATABASE_URI'] = conexao
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+from database import db
+from flask_migrate import Migrate
+from models import Usuario, Pizza, Pedido
+db.init_app(app)
+migrate = Migrate(app, db)
+from modulos.usuarios import bp_usuario
+app.register_blueprint(bp_usuario, url_prefix='/usuarios')
+
+
